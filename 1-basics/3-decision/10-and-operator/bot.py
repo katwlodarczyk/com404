22ac41079177a52fcf61b04e1062293d25c8a955
Kat Wlodarczyk <katbwlodarczyk@gmail.com>
2019-10-03 10:33:12 +0000

    and operator

---
+++ 1-basics/3-decision/10-and-operator/bot.py
@@ -0,0 +1,9 @@
+#and operator
+print("What did I hear?")
+what_hear = input()
+print("What did I see?")
+what_see = input()
+if((what_hear == "grr") and (what_see == "two red eyes")):
+    print("There is a scary creature. I should get out of here!")
+else:
+    print("I am a little scared but I will continue")
