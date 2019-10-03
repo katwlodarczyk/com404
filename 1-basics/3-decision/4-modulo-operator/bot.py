b61b0976c9ac0d260d955554d26de59496c04046
Kat Wlodarczyk <katbwlodarczyk@gmail.com>
2019-10-03 09:00:02 +0000

    modulo

---
+++ 1-basics/3-decision/4-modulo-operator/bot.py
@@ -0,0 +1,6 @@
+print("Please enter a whole number.")
+number = int(input())
+if (number % 2 == (number <0)):
+    print ("the number " +str(number)+ " is even")
+else: 
+    print("the number " +str(number)+ " is odd")
