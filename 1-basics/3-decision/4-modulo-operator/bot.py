508f1d690960cb19b55124def6922dd9e388df4e
Kat Wlodarczyk <katbwlodarczyk@gmail.com>
2019-10-03 09:15:01 +0000

    comparisson operators

--- 1-basics/3-decision/4-modulo-operator/bot.py
+++ 1-basics/3-decision/4-modulo-operator/bot.py
@@ -1,6 +1,6 @@
 print("Please enter a whole number.")
 number = int(input())
-if (number % 2 == (number <0)):
+if (number % 2 == 0):
     print ("the number " +str(number)+ " is even")
 else: 
     print("the number " +str(number)+ " is odd")
---
+++ 1-basics/3-decision/5-comparison-operators/bot.py
@@ -0,0 +1,13 @@
+#comparisson operators
+print("Please enter the first number.")
+first_number = int(input())
+print(str(first_number))
+print("Please enter the second number.")
+second_number = int(input())
+print(str(second_number))
+if (first_number < second_number):
+    print("The first number is the smallest!")
+elif (first_number > second_number):
+    print("The second number is the smallest!")
+else:
+    print("The both numbers are equal")
