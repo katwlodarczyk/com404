98fccb2b4d154042acb28759693d55b0a0f38c48
Kat Wlodarczyk <katbwlodarczyk@gmail.com>
2019-10-11 12:00:59 +0000

    to finish later

---
+++ 1-basics/4-repetition/3-nested-loop/2-nestings/bot.py
@@ -0,0 +1,8 @@
+print("enter a sequence of characters consisting of dashes and two markers")
+sequence = input()
+print("enter a character representing the marker")
+marker= input()
+#letter= "-"
+
+for letter in sequence:
+    print(letter)
