441d84c21191ce0a7198a4355ef8e9257225bb2e
Kat Wlodarczyk <katbwlodarczyk@gmail.com>
2019-10-17 07:47:23 +0000

    the end of task 7

--- 1-basics/4-repetition/3-nested-loop/2-nestings/bot.py
+++ 1-basics/4-repetition/3-nested-loop/2-nestings/bot.py
@@ -2,7 +2,17 @@ print("enter a sequence of characters consisting of dashes and two markers")
 sequence = input()
 print("enter a character representing the marker")
 marker= input()
-#letter= "-"
 
-for letter in sequence:
-    print(letter)
+marker1_position= -1
+marker2_position= -1
+
+for position in range(0, len(sequence), 1):
+    letter = sequence[position]
+
+    if (letter == marker):
+        if (marker1_position == -1):
+            marker1_position = position
+        else: 
+            marker2_position = position
+
+print("The distance between the markers is", marker2_position - marker1_position -1)
